73df879a97db0e4e6b2211bd0071dfcba64dbe61
Deviant <michaelngo1337@yahoo.com>
2021-05-20 15:23:27 -0700

    got localStorage objects to display on site

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,12 @@
+if(localStorage.length) {
+  for (let i = 9; i <= 17; i++) {
+    $('#hour-' + i + ' .description').val(localStorage.getItem('hour-' + i))
+  }
+}
+
+
 document.addEventListener('click', event => {
   if(event.target.classList.contains('saveBtn')) {
-    //key needs to be unique
-    localStorage.setItem('text', $(event.target).siblings('.description').val())
+    localStorage.setItem(event.target.parentNode.id, $(event.target).siblings('.description').val())
   }
 })
